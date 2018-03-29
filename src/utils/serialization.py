49c70ababc5937678f0726b40de13b4d8b9c97e5
Lukasz Haze <lhaze@lhaze.name>
2018-03-29 02:38:43 +0200

    utils: serialization with YAML
    * YAML `include` directive (based on https://gist.github.com/joshbode/569627ced3076931b02f)
    * out with explicite ruamel.yaml imports

---
+++ src/utils/serialization.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+
+from ruamel import yaml
+from typing import Any, IO
+
+
+class ExtLoader(yaml.Loader):
+    """YAML Loader with `!include` constructor."""
+
+    def __init__(self, stream: IO, *args, **kwargs) -> None:
+        """Initialise Loader."""
+        try:
+            self._root = os.path.split(stream.name)[0]
+        except AttributeError:
+            self._root = os.path.curdir
+
+        super().__init__(stream, *args, **kwargs)
+
+
+def construct_include(loader: ExtLoader, node: yaml.Node) -> Any:
+    """Include file referenced at node."""
+
+    filename = os.path.abspath(os.path.join(loader._root, loader.construct_scalar(node)))
+    extension = os.path.splitext(filename)[1].lstrip('.')
+
+    with open(filename, 'r') as f:
+        if extension in ('yaml', 'yml'):
+            return yaml.load(f, ExtLoader)
+        elif extension in ('json', ):
+            return json.load(f)
+        else:
+            return ''.join(f.readlines())
+
+
+yaml.add_constructor('!include', construct_include, ExtLoader)
+
+
+def load(stream):
+    """
+    Own YAML-deserialization based on:
+        * ruamel.yaml (some additional bugfixes vs regular PyYaml module)
+        * unsafe loading (be sure to use it only for own datafiles)
+        * YAML inclusion feature
+    """
+    return yaml.load(stream, Loader=ExtLoader)
+
+
+def load_from_filename(filename):
+    with open(filename, 'r') as f:
+        return load(f)
