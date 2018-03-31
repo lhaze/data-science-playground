128b168f602529eb32992572b1ba532cf33a7a4c
Lukasz Haze <lhaze@lhaze.name>
2018-03-31 03:22:21 +0200

    utils/rising_sun: get models by name
    * modifications to import_all_names
    * get_model
    * is_model

--- src/rising_sun/models/__init__.py
+++ src/rising_sun/models/__init__.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
-from utils.imports import import_all_names
+from utils.imports import get_all_names
 from utils.serialization import load_from_filename
 from utils.oss import dirname, path
 
-import_all_names(__file__, __name__)
+from .base import SimpleModel
+
+
+def is_model(cls):
+    try:
+        return issubclass(cls, SimpleModel)
+    except TypeError:
+        return False
+
+
+__models__ = get_all_names(__file__, __name__, is_model)
+
+
+def get_model(name):
+    return __models__.get(name)
 
 
 def sample():
--- src/utils/imports.py
+++ src/utils/imports.py
@@ -3,16 +3,17 @@ import os
 import sys
 
 
-def import_all_names(_file, _name):
+def get_all_names(_file, _name, _filter=lambda obj: True):
     """
-    Util for a tricky dynamic import of all names from all submodules.
+    Util for returning from all submodules all objects fulfilling the `_filter` condition.
     Use it in the __init__.py using following idiom:
-        import_all_names(__file__, __name__)
+        a_dict = get_all_names(__file__, __name__, a_condition)
     Supports __all__ attribute of the submodules.
     """
     path = os.path.dirname(os.path.abspath(_file))
     parent_module = sys.modules[_name]
 
+    result = {}
     for py in [filename[:-3] for filename in os.listdir(path)
                if filename.endswith('.py') and filename != '__init__.py']:
         module = __import__('.'.join([_name, py]), fromlist=[py])
@@ -26,9 +27,11 @@ def import_all_names(_file, _name):
             if hasattr(parent_module, name) and \
                getattr(parent_module, name) is not obj:
                 msg = (
-                    "Function import_all_names hit upon conflicting "
+                    "Function get_all_names hit upon conflicting "
                     "names. '{0}' is already imported to {1} module."
                 ).format(name, module)
                 import warnings
                 warnings.warn(msg)
-            setattr(parent_module, name, obj)
+            if _filter(obj):
+                result[name] = obj
+    return result
