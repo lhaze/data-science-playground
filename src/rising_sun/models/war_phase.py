e6f0e2cb2c6d326416dd806c6ec18f19c4607ae6
Lukasz Haze <lhaze@lhaze.name>
2018-04-24 02:41:38 +0200

    Refs #14. rising_sun: War Advantages & config for full game

---
+++ src/rising_sun/models/war_phase.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column, ForeignKey, Integer, Unicode
+from sqlalchemy.orm import relationship, validates
+
+from rising_sun.models.base import config_repo, ConfigModel, DbModel
+from utils import validation as v
+from utils.functools import reify
+
+
+class AdvantageSchema(v.Schema):
+    name = v.SchemaNode(v.String(), validator=v.Length(max=20))
+    procedure = v.SchemaNode(v.String(), validator=v.Length(max=20))
+
+
+class Advantage(ConfigModel):
+    yaml_tag = 'advantage'
+    _pk_keys = ('context', 'name')
+    __schema__ = AdvantageSchema()
+
+
+class AdvantageBid(DbModel):
+    __tablename__ = 'advantage_bid'
+    _pk_keys = ('context', 'name')
+
+    advantage_name = Column(Unicode(20), primary_key=True)
+    clan_name = Column(Unicode, ForeignKey('clan.name'), primary_key=True)
+    clan = relationship("Clan")
+    coins = Column(Integer)
+
+    @reify
+    def advantage(self):
+        return config_repo.get("Advantage", (self.context, self.advantage_name))
